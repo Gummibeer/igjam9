d690280431879e4fa76361a1f1c30e5ecb41edeb
Tom Witkowski <dev.gummibeer@gmail.com>
2015-11-06 23:31:56 +0100

    use socket.io

--- src/js/server/main.js
+++ src/js/server/main.js
@@ -1,12 +1,24 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 var port = 666;
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/html/index.html');
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('a user disconnected');
+    });
+
+    socket.on('joinLobby', function(name){
+        console.log('user joined lobby: ' + name);
+    });
 });
 
 http.listen(port, function () {
     console.log('listening on *:' + port);
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/html/index.html');
 });
