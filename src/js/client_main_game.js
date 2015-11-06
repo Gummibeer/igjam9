4adf275b97606312de8f58e5ad4cf313ea13e90d
jxl1983 <mike.gehrhardt@gmx.de>
2015-11-06 22:14:32 +0100

    - wip

--- src/js/client_main_game.js
+++ src/js/client_main_game.js
@@ -1,10 +1,12 @@
 var IggjGame = function() {
 
     var _socket = null;
+    var _mainStage = null;
 
     var _init =function() {
         console.log('game init started');
         _socket = io();
+
     };
 
     _init();
---
+++ src/js/stageHandler.js
@@ -0,0 +1,11 @@
+var IggjStageHandler = function() {
+
+    var _stage = null;
+    var _currentDisplayObject = null;
+
+    var _init = function() {
+        _stage = $('#mainStage');
+    };
+
+    _init();
+};
