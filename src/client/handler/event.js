9a5c6eba9e76b77345ea5741cb68ac66d1804041
jxl1983 <mike.gehrhardt@gmx.de>
2015-11-07 01:14:37 +0100

    - wip

--- src/client/handler/event.js
+++ src/client/handler/event.js
@@ -1,12 +1,18 @@
-var IggjEventHandler = function () {
-    var mySubscriber = new Object();
-    this.subscribe = function (event, callback) {
-        mySubscriber.subscribe(event, callback);
-    };
-    this.publish = function (event) {
-        mySubscriber.trigger(event)
-    };
-    this.unsubscribe = function (event, callback) {
-        mySubscriber.unsubscribe(event, callback)
+var topics = {};
+var IggjEventHandler =  function( id ) {
+    var callbacks, method,
+        topic = id && topics[ id ];
+
+    if ( !topic ) {
+        callbacks = jQuery.Callbacks();
+        topic = {
+            publish: callbacks.fire,
+            subscribe: callbacks.add,
+            unsubscribe: callbacks.remove
+        };
+        if ( id ) {
+            topics[ id ] = topic;
+        }
     }
+    return topic;
 };
