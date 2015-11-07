a481f517b5c6deb8489de1c2815e7fb211a1226c
jxl1983 <mike.gehrhardt@gmx.de>
2015-11-07 01:28:31 +0100

    - wip

--- src/client/screens/lobby.js
+++ src/client/screens/lobby.js
@@ -1,3 +1,26 @@
-IggjLobbyScreen = function () {
-    console.log('CREATE LOBBY');
+IggjLobbyScreen = function (stageHandler ,eventHandler, networkHandler) {
+
+    var socket = null;
+
+    var _init = function() {
+        console.log('CREATE LOBBY');
+        socket = networkHandler.getNetworkSocket();
+        _initListeners();
+        _joinLobby();
+    };
+
+    var _initListeners = function() {
+        socket.on('userList', _onLobbyJoined);
+    };
+
+    var _onLobbyJoined = function(lobbyData) {
+        console.log(lobbyData)
+    };
+
+    var _joinLobby = function(){
+        console.log('Request Lobby Login')
+        socket.emit('joinLobby', localStorage.getItem('username'));
+    };
+
+    _init();
 };
