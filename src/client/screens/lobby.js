56bf5b2fcf781b42e43213c57ed6d48d63e6da6c
Tom Witkowski <dev.gummibeer@gmail.com>
2015-11-07 03:48:20 +0100

    change to socket.id

--- src/client/screens/lobby.js
+++ src/client/screens/lobby.js
@@ -89,7 +89,7 @@ IggjLobbyScreen = function (stageHandler ,eventHandler, networkHandler) {
 
     var _joinLobby = function(){
         console.log('Request Lobby Login');
-        _socket.emit('joinLobby', {id: _myId,name: _myName});
+        _socket.emit('joinLobby', _myName);
     };
 
     this.destroy = function() {
--- src/server/helpers.js
+++ src/server/helpers.js
@@ -6,6 +6,17 @@ exports.getUserData = function (user) {
 };
 
 exports.getUsersList = function (users) {
+    return Object.keys(users).map(function(key) {
+        var user = users[key];
+        if (user.status == 1) {
+            return exports.getUserData(user);
+        } else {
+            return null;
+        }
+    }).filter(function (item) {
+        return !(item === null);
+    });
+
     return users.map(function (user) {
         if (user.status == 1) {
             return exports.getUserData(user);
