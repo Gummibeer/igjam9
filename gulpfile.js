a4ba63af9051d6fd031d1aaabbc6351f23a432fe
Tom Witkowski <dev.gummibeer@gmail.com>
2015-11-07 07:45:19 +0100

    use gulp minify css

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var minifyCSS = require('gulp-minify-css');
 
 gulp.task('less', function () {
     return gulp.src('./src/less/main.less')
         .pipe(less({
             paths: [ path.join(__dirname, 'less', 'includes') ]
         }))
+        .pipe(minifyCSS())
         .pipe(gulp.dest('./src/css'));
 });
